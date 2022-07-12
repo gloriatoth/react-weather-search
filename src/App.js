059787807781ee423884f245598b80f0d26a4d77
Gloria Toth <gloria.toth@emilfrey.ch>
2022-07-12 09:27:10 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by{" "}
+        <a
+          href="https://astounding-nasturtium-470d21.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          gloria
+        </a>{" "}
+        and is{" "}
+        <a
+          href="httfooters://github.com/gloriatoth/react-weather-search"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
