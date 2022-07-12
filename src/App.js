a59169565cd5f239c8ba81ff4453bc96ab2889ab
Gloria Toth <gloria.toth@emilfrey.ch>
2022-07-12 16:45:38 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://astounding-nasturtium-470d21.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          gloria
-        </a>{" "}
-        and is{" "}
-        <a
-          href="httfooters://github.com/gloriatoth/react-weather-search"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <button className="btn btn-primary">test</button>
+
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://astounding-nasturtium-470d21.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            gloria
+          </a>{" "}
+          and is{" "}
+          <a
+            href="httfooters://github.com/gloriatoth/react-weather-search"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
