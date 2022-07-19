b66ff102dceb67f8f946a933bf1f6c960225d10b
Gloria Toth <gloria.toth@emilfrey.ch>
2022-07-19 09:08:52 +0200

    Fixed and optimized

---
+++ src/WeatherForecast.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
+import axios from "axios";
+
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    // console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "39c5c2fcc0fa0e5e619ee9967341c782";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
+}
